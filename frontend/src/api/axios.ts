475ae0455e56dca2a5978221ecc63746ddc8dbe5
Yash Chandravanshi <yash0111chandravanshi@gmail.com>
2026-02-17 22:42:03 +0530

    update backend url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1";
-
+const BASE_URL = "https://slipper-ecommerce.onrender.com/api/v1";
+0
 
 export const publicClient = axios.create({
   baseURL: BASE_URL,
