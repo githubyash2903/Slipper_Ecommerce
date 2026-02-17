5cd72730914cc3474730dcf0cf0dcde2fc5008a1
Yash Chandravanshi <yash0111chandravanshi@gmail.com>
2026-02-17 22:33:47 +0530

    update package.json

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,15 +1,24 @@
 import { Pool } from "pg";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
-  connectionString: process.env.DATABASE_URL,
-});
+const isProduction = process.env.NODE_ENV === "production";
+console.log(process.env.DATABASE_URL, process.env.NODE_ENV)
+const pool = isProduction
+  ? new Pool({
+  
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false, 
+      },
+    })
+  : new Pool({
+      host: process.env.DB_HOST || "localhost",
+      database: process.env.DB_DATABASE,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT) || 5432,
+    });
 
 export default pool;
